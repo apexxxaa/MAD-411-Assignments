775a65c8b2f57342aa55dec837e78bdf89b78e05
Apekshya Khanal <W0842585@stclaircollege.ca>
2025-03-20 21:36:08 -0400

    "Created layout with expense name and amount and add expense button"

---
+++ app/src/main/java/com/example/myapplication/Expense.kt
@@ -0,0 +1,3 @@
+package com.example.myapplication
+
+data class Expense(val name: String, val amount: Double)
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,16 +4,22 @@ package com.example.myapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
 
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var inputBox: EditText
-    private lateinit var greetingTextView: TextView
+    private lateinit var etExpenseNameId: EditText
+    private lateinit var etEnterAmount: EditText
+    private lateinit var btnAddExpense: Button
+    private lateinit var recyclerView: RecyclerView
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,12 +27,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-       inputBox = findViewById(R.id.inputBox)
-        greetingTextView = findViewById(R.id.greetingTextView)
+       etExpenseNameId = findViewById(R.id.etExpenseNameId)
+       etEnterAmount = findViewById(R.id.etEnterAmount)
+       btnAddExpense = findViewById(R.id.btnAddExpense)
+        recyclerView = findViewById(R.id.recyclerView)
+
+        recyclerView.layoutManager =  LinearLayoutManager(this)
+
+        btnAddExpense.setOnClickListener{
+            val name = etExpenseNameId.text.toString()
+            val amountText = etEnterAmount.text.toString()
+
+            if(name.isNotEmpty() && amountText.isNotEmpty()){
+                val amount = amountText.toDouble()
+
+            }
+        }
+
     }
 
-    fun onClick(view: View){
-        val name = inputBox.text.toString()
-        greetingTextView.text = "Hello, $name !!!!! :)"
-    }
+
 }
